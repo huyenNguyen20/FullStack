0b7e6a99aa459f8299eb3447feac411b85ec861b
Huyen Nguyen <nguyendieuhuyen2809@gmail.com>
2020-09-09 14:55:55 -0500

    Node MongoDB Part 2

--- Backend/node-mongo/index.js
+++ Backend/node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
+const dboper = require("./operations.js");
 
 //Connection URL
 const url = 'mongodb://localhost:27017';
@@ -15,22 +16,34 @@ MongoClient.connect(url, (err, client) => {
     const db = client.db(dbName);
 
     const collection = db.collection("dishes");
-    collection.insert({"name": "pizzaria", "description": "test"}, (err, result) => {
-        assert.equal(err, null);
+   
+    dboper.insertDocument(db, 
+        {name: "Pasta", description : "Test"},
+        "dishes", 
+        (result) => {
+        console.log(`Insert Document: \n ${result.ops}`);
 
-        console.log("After insert: \n");
-        console.log(result.ops);
-        
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err, null);
+        dboper.findDocuments(db, 
+            "dishes", 
+            (docs) => {
+            console.log(`Found Documents: \n ${docs}`);
 
-            console.log("Found: \n");
-            console.log(docs);
+            dboper.updateDocument(db, 
+                {name: "Pasta"}, 
+                {description: "Updated Test"},
+                "dishes", 
+                (result) => {
+                    console.log(`Update Documents: ${result.result}`);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err, null);
+                    dboper.findDocuments(db, "dishes", (result) => {
+                        console.log(`Found Document:  ${result}`);
 
-                client.close();
+                        db.dropCollection("dishes", (result) => {
+                            console.log(`Dropped Collection ${result}`);
+                            
+                            client.close();
+                        })
+                })
             })
         })
     })

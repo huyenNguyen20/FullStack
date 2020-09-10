1670d952bdbea0769dacbb0643fb4d052c60fb85
Huyen Nguyen <nguyendieuhuyen2809@gmail.com>
2020-09-10 11:08:48 -0500

    React Router

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from "./App";
-
 
 
 ReactDOM.render(<App />, document.getElementById("root"))

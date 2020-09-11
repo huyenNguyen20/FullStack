62a8a7f952c99a34044cf9a722ca69218ff1754e
Huyen Nguyen <nguyendieuhuyen2809@gmail.com>
2020-09-11 10:49:59 -0500

    Single Page Application Part 2

--- React/my-app/src/components/DishDetailComponent.js
+++ React/my-app/src/components/DishDetailComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Card, CardImg, CardText, CardBody,
-    CardTitle,  Media} from "reactstrap";
+    CardTitle,  Media, Breadcrumb, BreadcrumbItem} from "reactstrap";
+import {Link} from "react-router-dom";
 
 function RenderDish({dish}) {
     return (
@@ -60,13 +61,27 @@ function DishDetail (props) {
         return(
             <div className="container">
                     <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                       <RenderDish dish={props.dish} />
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <Link to="/menu">Menu</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>
+                                {props.dish.name}
+                            </BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
                     </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.dish.comments} />
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={props.comments} />
+                        </div>
                     </div>
-                </div>
             </div>
             
         );

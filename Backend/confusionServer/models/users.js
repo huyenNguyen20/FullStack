501d67d3f1c440fd597c3ca52462c5ad9e3634d9
Huyen Nguyen <nguyendieuhuyen2809@gmail.com>
2020-09-21 10:00:53 -0500

    Mongoose Population

--- Backend/confusionServer/models/users.js
+++ Backend/confusionServer/models/users.js
@@ -3,6 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 var User = new Schema({
+    firstname: {
+        type: String,
+        default: ""
+    },
+    lastname: {
+        type: String,
+        default: ""
+    },
     admin:   {
         type: Boolean,
         default: false

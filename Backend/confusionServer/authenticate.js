4e02f6dde8f65bc61a8720c115a64f9eb66425f1
Huyen Nguyen <nguyendieuhuyen2809@gmail.com>
2020-09-25 20:02:52 -0500

    OAuth Passport-facebook

--- Backend/confusionServer/authenticate.js
+++ Backend/confusionServer/authenticate.js
@@ -5,6 +5,7 @@ var JwtStrategy = require("passport-jwt").Strategy;
 var ExtractJwt = require("passport-jwt").ExtractJwt;
 var jwt = require("jsonwebtoken"); // Used to create, sign, and verify tokens
 var config = require("./config.js");
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 exports.getToken = function(user) {
     return jwt.sign(user, config.secretKey, {expiresIn: 3600});
@@ -43,3 +44,30 @@ exports.verifyAdmin = (req, res, next) => {
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
+}
+));
